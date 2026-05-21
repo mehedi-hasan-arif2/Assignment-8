eeaa669fcab15551da28ca2c4151fbb3879f6080
Mehedi Hasan Arif <iam.mehediarif@gmail.com>
2026-05-21 14:40:16 +0600

    Update layout.js

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -1,11 +1,8 @@
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
 import { Toaster } from 'sonner';
 
 export default function MainLayout({ children }) {
   return (
     <>
-      <Navbar />
       <main className="pt-20">{children}</main>
       <Toaster position="top-right" richColors />
     </>
